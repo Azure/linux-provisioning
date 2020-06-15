d9d5bec46b15cc2f62b31ddce0a8d3e6f563568a
Thomas Stringer <thstring@microsoft.com>
2020-06-15 15:41:41 -0400

    refactor base pa

---
+++ base-provisioning-agent/azure_provisioning.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -euo pipefail
+
+VMID_FILE="/var/local/vmid"
+REPORT_READY_SCRIPT="/usr/local/report_ready.py"
+
+run_provisioning () {
+    echo "Reporting ready to Azure"
+    python3 "$REPORT_READY_SCRIPT"
+
+    local NEW_HOSTNAME=$(curl \
+        -H 'metadata: true' \
+        'http://169.254.169.254/metadata/instance/compute/name?api-version=2019-06-01&format=text')
+    echo "Setting new hostname to $NEW_HOSTNAME"
+    hostnamectl set-hostname "$NEW_HOSTNAME"
+
+    echo "Provisioning complete"
+}
+
+echo "Running conditional Azure Linux provisioning"
+
+CURRENT_VMID=$(curl \
+    -H 'metadata: true' \
+    'http://169.254.169.254/metadata/instance/compute/vmId?api-version=2019-06-01&format=text')
+
+# If the vmId marker file does not exist at all, then this means
+# that provisioning was never run. In that case, run provisioning.
+if [[ ! -f "$VMID_FILE" ]]; then
+    echo "No vmid file, running provisioning"
+    run_provisioning
+    echo "$CURRENT_VMID" > "$VMID_FILE"
+    exit
+fi
+
+# If the vmId file exists, check to see if the recorded vmId is
+# matching the current actual vmId of the machine. If they are
+# the same, then this means that provisioning has already run for
+# this VM. But if the vmIds do not match, then we need to treat
+# this boot as requiring provisioning steps.
+PREVIOUS_VMID=$(cat "$VMID_FILE")
+echo "Previous vmId: $PREVIOUS_VMID"
+echo "Current vmId: $CURRENT_VMID"
+if [[ "$PREVIOUS_VMID" != "$CURRENT_VMID" ]]; then
+    echo "vmId does not match, running provisioning"
+    run_provisioning
+    echo "$CURRENT_VMID" > "$VMID_FILE"
+else
+    echo "Matching vmIds, not running provisioning"
+fi
---
+++ base-provisioning-agent/report_ready.py
@@ -0,0 +1,89 @@
+"""Python module to easily allow for reporting ready to Azure."""
+
+import http.client
+from xml.etree import ElementTree
+
+def get_vm_identifiers(wireserver_connection):
+    """Get the ContainerId and InstanceId from the Wireserver."""
+
+    wireserver_connection.request(
+        'GET',
+        '/machine?comp=goalstate',
+        headers={'x-ms-version': '2012-11-30'}
+    )
+
+    resp = wireserver_connection.getresponse()
+
+    wireserver_goalstate = resp.read().decode('utf-8')
+
+    xml_el = ElementTree.fromstring(wireserver_goalstate)
+
+    container_id = xml_el.findtext('Container/ContainerId')
+    instance_id = xml_el.findtext('Container/RoleInstanceList/RoleInstance/InstanceId')
+
+    return dict(container_id=container_id, instance_id=instance_id)
+
+def construct_report_ready_xml(container_id, instance_id):
+    """Construct the XML response we need to send to Wireserver to report ready."""
+
+    health = ElementTree.Element('Health')
+    goalstate_incarnation = ElementTree.SubElement(health, 'GoalStateIncarnation')
+    goalstate_incarnation.text = '1'
+    container = ElementTree.SubElement(health, 'Container')
+    container_id_el = ElementTree.SubElement(container, 'ContainerId')
+    container_id_el.text = container_id
+    role_instance_list = ElementTree.SubElement(container, 'RoleInstanceList')
+    role = ElementTree.SubElement(role_instance_list, 'Role')
+    instance_id_el = ElementTree.SubElement(role, 'InstanceId')
+    instance_id_el.text = instance_id
+    health_second = ElementTree.SubElement(role, 'Health')
+    state = ElementTree.SubElement(health_second, 'State')
+    state.text = 'Ready'
+
+    out_xml = ElementTree.tostring(
+        health,
+        encoding='unicode',
+        method='xml'
+    )
+    return out_xml
+
+def report_ready():
+    """Make the report ready HTTP request to Wireserver."""
+
+    wireserver_ip = '168.63.129.16'
+
+    try:
+        wireserver_connection = http.client.HTTPConnection(wireserver_ip)
+
+        print('Retrieving goal state from the Wireserver')
+        vm_identifiers = get_vm_identifiers(wireserver_connection=wireserver_connection)
+        print(f'ContainerId: {vm_identifiers["container_id"]}')
+        print(f'InstanceId: {vm_identifiers["instance_id"]}')
+
+        ready_xml = construct_report_ready_xml(
+            container_id=vm_identifiers['container_id'],
+            instance_id=vm_identifiers['instance_id']
+        )
+        print('Sending the following data to Wireserver:')
+        print(ready_xml)
+
+        wireserver_connection.request(
+            'POST',
+            '/machine?comp=health',
+            headers={
+                'x-ms-version': '2012-11-30',
+                'Content-Type': 'text/xml;charset=utf-8',
+                'x-ms-agent-name': 'custom-provisioning'
+            },
+            body=ready_xml
+        )
+
+        resp = wireserver_connection.getresponse()
+        print(f'Response: {resp.status} {resp.reason}')
+    except http.client.HTTPException as http_exception:
+        print(f'Error communicating with Wireserver: {http_exception}')
+    finally:
+        wireserver_connection.close()
+
+if __name__ == '__main__':
+    report_ready()
